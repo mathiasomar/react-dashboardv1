207ac0fbc4721396455cab13cd1cd8afe02193af
mathiasomar <ommykmathy@gmail.com>
2024-09-29 10:00:37 +0300

    page header

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,9 +1,14 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
-    </div>
-  )
-}
+    <Box m="20px">
+      <Box display="flex" alignContent="center" justifyContent="space-between">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+    </Box>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
